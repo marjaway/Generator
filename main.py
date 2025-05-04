8bff34ba2200a4191d6451052e247d0fb7223531
marjaway <aleks17124@gmail.com>
2025-05-04 16:10:25 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from faker import Faker
 import time
 import g4f
 import segno
+import banner
 from colorama import init
 from colorama import Fore, Back, Style
 init()
@@ -12,45 +13,20 @@ init()
 for i in tqdm(range(100), desc="Загрузка"):
     time.sleep(0.03)
 
-
 faker = Faker("RU_ru")
 
-
-print(Fore.CYAN + '''\n				  █▀▀ █▀▀ █▄░█ █▀▀ █▀█ ▄▀█ ▀█▀ █▀█ █▀█ - version 0.1.9
-				  █▄█ ██▄ █░▀█ ██▄ █▀▄ █▀█ ░█░ █▄█ █▀▄''')
+def main():
+    input(Fore.RESET + "\nНажмите Enter, чтобы вернуться в меню...")
 				  
 
 while True:
-    print(Fore.RESET + '''
-	   					  ║
-						  ║
-	  					  ║	 
-	 ╔════════════════════════════════════════╬══════════════════════════════════════╗
-	 ║					  ║           				 ║
-	 ║  					  ║					 ║
-	 ║ Генератор           		Генерация для интернета   	      Другое     ║
- ╔════════════════════════════════╗ ╔════════════════════════════════╗ ╔═════════════════════════════════╗
- ║ [ 1 ] - Информация о софте 	  ║ ║ [ 7 ] - Генератор банк. карт   ║ ║ [ 13 ] - Генератор улиц 	 ║
- ║ [ 2 ] - Рандомайзер цифр       ║ ║ [ 8 ] - Генератор паролей      ║ ║ [ 14 ] - Генератор городов      ║
- ║ [ 3 ] - Генератор ру имён	  ║ ║ [ 9 ] - Генератор ссылок	     ║ ║ [ 15 ] - Генератор профессий    ║
- ║ [ 4 ] - Генератор фамилий	  ║ ║ [ 10 ] - Генератор ников       ║ ║ [ 16 ] - Генератор анекдотов	 ║
- ║ [ 5 ] - Генератор личностей	  ║ ║ [ 11 ] - Генератор почт	     ║ ║ [ 17 ] - ChatGPT 4	 	 ║
- ║ [ 6 ] - Рандомайзер от 1 до 10 ║ ║ [ 12 ] - Генератор тел.номеров ║ ║ [ 18 ] - Генератор QR-кодов     ║
- ╚════════════════════════════════╝ ╚════════════════════════════════╝ ╚═════════════════════════════════╝
- ╔═════════════════════════════════╗╔═════════════════════════════════╗╔═════════════════════════════════╗
- ║ [ 19 ] - Выход из программы     ║║                                 ║║                                 ║  
- ║                                 ║║                                 ║║                                 ║
- ║                                 ║║                                 ║║                                 ║                     
- ║                                 ║║                                 ║║                                 ║
- ║                                 ║║                                 ║║                                 ║
- ╚═════════════════════════════════╝╚═════════════════════════════════╝╚═════════════════════════════════╝''')
-
+    banner.banner()
 
     command = input(Fore.CYAN + "					[ ? ] -> Введите число: " + Fore.RESET)
     print("\n")
 
     if command == "1":
-        print('''	Автор tg: @marjaway / discord: marjaway
+        print('''	Автор | tg: @marjaway / discord: marjaway
 	Версия софта -> 0.2.0
 	Автор не несёт ответственность за эту программу :). Всё что генерирует программа это вымошленно!''')
         news = input("\nПосмотреть историю обновлений (Напиши update): ")
@@ -69,16 +45,18 @@ while True:
         0.1.7 - Добавлен Генератор городов / Генератор профессий
         0.1.8 - Добавление загрузки при заходе в программу / Изменён порядок функций / Генератор анекдотов
         0.1.9 - Добавлен ChatGPT
-        0.2.0 - Добавлен Генератор QR-кодов / Добавлены новые блоки функций''')
+        0.2.0 - Добавлен Генератор QR-кодов / Добавлены новые блоки функций
+        0.2.1 - Переделан Генартор QR-кодов / Интерфейс перенесен в другой файл''')
 
-        input(Fore.RESET + "\nНажмите Enter, чтобы вернуться в меню\n")
+        main()
         
 
     elif command == "2":
+
         time.sleep(0.2)
         print(Fore.RESET + "\nРандомные числа от 0 до 9999999999\n")
         time.sleep(0.1)
-        randomm = input("Сколько чисел хотите вывести: ")
+        randomm = int(input("Сколько чисел хотите вывести: "))
         randomm = int(randomm) 
             
         for h in range(randomm):
@@ -86,12 +64,13 @@ while True:
             print(h)
             time.sleep(0.1)
 
-        input(Fore.RESET + "\nНажмите Enter, чтобы вернуться в меню...")
+        main()
         
 
     elif command == "3":
+
         print(Fore.RESET + "\nГенерация имён\n")
-        name = input("Сколько имён хотите сгенерировать: ")
+        name = int(input("Сколько имён хотите сгенерировать: "))
         time.sleep(0.1)
         name = int(name)
             
@@ -99,27 +78,28 @@ while True:
             print(faker.name())
             time.sleep(0.1)
 
-        input(Fore.RESET + "\nНажмите Enter, чтобы вернуться в меню")
+        main()
         
 
     elif command == "4":
+        
         time.sleep(0.3)
         print(Fore.RESET + "\nГенерация фамилии")
         
-        fam = input("Количество фамилий: ")
+        fam = int(input("Количество фамилий: "))
         time.sleep(0.1)
         fam = int(fam)
         for f in range(fam):
             print(faker.last_name())
             time.sleep(0.1)
 
-        input(Fore.RESET + "\nНажмите Enter, чтобы вернуться в меню")
+        main()
         
 
     elif command == "5":
         time.sleep(0.2)
         print(Fore.RESET + "\nГенератор личностей...")
-        profe = input("\nСколько хотите сгенерировать: ")
+        profe = int(input("\nСколько хотите сгенерировать: "))
         profe = int(profe)
         
         for a in range(profe):
@@ -135,145 +115,149 @@ while True:
             print("Работа: " + faker.job())
             time.sleep(0.1)
 
-        input(Fore.RESET + "\nНажмите Enter, чтобы вернуться в меню")
+        main()
         
 
     elif command == "6":
         time.sleep(0.2)
         numbers = random.randint(1, 10)
         print(numbers)
-        input(Fore.RESET + "\nНажмите Enter, чтобы вернуться в меню")	
+        
+        main()	
         
             
     elif command == "7":
         time.sleep(0.2)
         print(Fore.RESET + "\nГенерация банковских карт\n")
-        m = input("Сколько хотите сгенерировать номеров банк. карты: ")
-        m = int(m)
-        for i in range(m):
+        bank = int(input("Сколько хотите сгенерировать номеров банк. карты: "))
+        bank = int(bank)
+        for i in range(bank):
             a = random.randint(0000000000000000, 9999999999999999)
             u = random.randint(000, 999)
             print("\n")
+
             print(a)
             time.sleep(0.1)
+
             print(u)
             time.sleep(0.1)
+
             print(faker.name())
             time.sleep(0.1)
 
-        input(Fore.RESET + "\nНажмите Enter, чтобы вернуться в меню")
+        main()
         
 
     elif command == "8":
         time.sleep(0.2)
         print(Fore.RESET + "Генерация пароля...\n")
         chars = "+-/*!&$#?=@<>abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-        number = input("Количество паролей: ")
-        length = input("Длинна пароля: ")
+        number = int(input("Количество паролей: "))
+        passw = int(input("Длинна пароля: "))
         print("\n")
         number = int(number)
-        length = int(length)
+        passw = int(passw)
         for n in range(number):
             password=''
-            for i in range(length):
+            for i in range(passw):
                 password += random.choice(chars)
             print(password)
             time.sleep(0.1)
         
-        input(Fore.RESET + "\nНажмите Enter, чтобы вернуться в меню")
+        main()
         
 
     elif command == "9":
         time.sleep(0.2)
         print(Fore.RESET + "\nГенерация ссылок")
-        web = input("\nСколько хотите сгенерировать ссылок: ")
+        web = int(input("\nСколько хотите сгенерировать ссылок: "))
         web = int(web)
             
         for g in range(web):
             print(faker.uri())
             time.sleep(0.1)
 
-        input(Fore.RESET + "\nНажмите Enter, чтобы вернуться в меню")
+        main()
         
 
     elif command == "10":
         time.sleep(0.2)
         print(Fore.RESET + "Генератор ников...")
         char = "abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-        nnumber = input("\nКоличество ников: ")
-        llengeth = input("Длинна ника: ")
+        nicks = int(input("\nКоличество ников: "))
+        niks1 = int(input("Длинна ника: "))
         print("\n")
-        nnumber = int(nnumber)
-        llengeth = int(llengeth)
-        for p in range(nnumber):
+        nicks = int(nicks)
+        niks1 = int(niks1)
+        for p in range(nicks):
             nick=''
-            for i in range(llengeth):
+            for i in range(niks1):
                 nick += random.choice(char)
             print(nick)
             time.sleep(0.1)
 
-        input(Fore.RESET + "\nНажмите Enter, чтобы вернуться в меню")
+        main()
 
     elif command == "11":
         time.sleep(0.2)
         print(Fore.RESET + "\nГенерация почт...\n")
-        numbber = input("Введите количесво почт: ")
-        numbber = int(numbber)
+        pochta = int(input("Введите количесво почт: "))
+        pochta = int(pochta)
 
-        for d in range(numbber):
+        for d in range(pochta):
             print(faker.ascii_free_email())
             time.sleep(0.1)
 
-        input(Fore.RESET + "\nНажмите Enter, чтобы вернуться в меню")
+        main()
         
 
     elif command == "12":
         time.sleep(0.2)
         print(Fore.RESET + "\nГенерация телефонных номеров...\n")
-        phone = input(Fore.CYAN + "Сколько хотите сгенерировать тел.номеров: ")
+        phone = int(input(Fore.CYAN + "Сколько хотите сгенерировать тел.номеров: "))
         phone = int(phone)
         
         for i in range(phone):
             print(faker.phone_number())
             time.sleep(0.1)
 
-        input(Fore.RESET + "\nНажмите Enter, чтобы вернуться в меню")
+        main()
         
             
     elif command == "13":
         time.sleep(0.2)
         print(Fore.RESET + "\nРандомайзер улиц\n")
-        ul = input("Количество улиц: ")
+        ul = int(input("Количество улиц: "))
         ul = int(ul)
         for h in range(ul):
             print(faker.street_address())
             time.sleep(0.1)
 
-        input(Fore.RESET + "\nНажмите Enter, чтобы вернуться в меню")
+        main()
         
             
     elif command == "14":
         time.sleep(0.2)
         print(Fore.RESET + "\nГенератор городов\n")
-        city = input("Количество городов: ")
+        city = int(input("Количество городов: "))
         city = int(city)
         for i in range(city):
             print(faker.city_name())
             time.sleep(0.1)
 
-        input(Fore.RESET + "\nНажмите Enter, чтобы вернуться в меню") 
+        main()
         
 
     elif command == "15":
         time.sleep(0.2)
         print(Fore.RESET + "\nРандомайзер профессий\n")
-        prof = input("Количество профессий: ")
+        prof = int(input("Количество профессий: "))
         prof = int(prof)
         for i in range(prof):
             print(faker.job())
             time.sleep(0.1)
 
-        input(Fore.RESET + "\nНажмите Enter, чтобы вернуться в меню")
+        main()
         
 
     elif command == "16":
@@ -285,19 +269,21 @@ while True:
             )
             return responce
         print(ask_gpt(f"Расскажи анекдот "))
-        input("Нажмите Enter, чтобы вернуться в меню...")
+
+        main()
         
 
     elif command == "17":
         n = input("Введите запрос (пишите сразу подробный запрос): ")
-        def ask_gpt(promt:str)->str:
+        def ask_gpt(promt:str) -> str:
             responce = g4f.ChatCompletion.create(
                 model=g4f.models.gpt_4,
                 messages=[{"role": "user", "content": promt}],
             )
             return responce
         print(ask_gpt(f"Пиши на русском, {n}"))
-        input("Нажмите Enter, чтобы вернуться в меню")
+        
+        main()
 
 
     elif command == "18":
@@ -306,10 +292,25 @@ while True:
 
         qrcod = input("Введите ссылку для QR-кода: ")
         qrcode = segno.make_qr(f"{qrcod}")
-        print("QR-код был сгенерирован. Сохраняется в папку где находится программа")
-        qrcode.save("qr-code.png") 
 
-        input("Нажмите Enter, чтобы вернуться в меню")   
+        print('''В каком формате сохранить?
+    1. PNG
+    2. SVG
+    3. PDF''')
+        photo = input("\n[ ? ] - Выберете формат: ")
+
+        if photo == "1":
+            qrcode.save("qr-code.png")
+        elif photo == "2":
+            qrcode.save("qr-code.svg")
+        elif photo == "3":
+            qrcode.save("qr-code.pdf")
+        else:
+            print(Fore.BLUE + "Нужно обязательно выбрать формат, в котором сохранится QR-cod")
+
+        print("QR-код был сгенерирован. Сохраняется в папку где находится программа")
+
+        main()   
 
 
     elif command == "19":
@@ -319,5 +320,5 @@ while True:
         
 
     else:
-        print(Fore.BLUE + "Ненайдена такая комманда8")
-        input(Fore.RESET + "Нажмите Enter, чтобы продолжить")
+        print(Fore.BLUE + "Ненайдена такая комманда")
+        main()
